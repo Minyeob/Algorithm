c26dbd40c3b759740432d3028b2c851331e54f16
Minyeob <nmy2511@gmail.com>
2016-10-15 23:24:27 +0900

    Merge sort practice

---
+++ Algorithm Practice/Algorithm Practice/11651.cpp
@@ -0,0 +1,185 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+struct point
+{
+	int x, y;
+};
+point a[100000];
+point b[100000];
+point c[100000];
+int location = 0;
+
+void merge(int start, int end)
+{
+	if (start == end)
+	{
+		return;
+	}
+
+	int mid = (start + end) / 2;
+
+	merge(start, mid);
+	merge(mid + 1, end);
+
+	int i = start;
+	int j = mid + 1;
+	int q = 0;
+
+	while (i <= mid && j <= end)
+	{
+		if (a[i].y < a[j].y)
+		{
+			b[q].x = a[i].x;
+			b[q].y = a[i].y;
+			++i;
+		}
+
+		else
+		{
+			b[q].x = a[j].x;
+			b[q].y = a[j].y;
+			++j;
+		}
+		q++;
+	}
+
+	while (i <= mid)
+	{
+		b[q].x = a[i].x;
+		b[q].y = a[i].y;
+		i++;
+		q++;
+	}
+
+	while (j <= end)
+	{
+		b[q].x = a[j].x;
+		b[q].y = a[j].y;
+		j++;
+		q++;
+	}
+
+	for (int i = start; i <= end; i++)
+	{
+		a[i].x = b[i - start].x;
+		a[i].y = b[i - start].y;
+	}
+}
+
+void merge_b(int start, int end)
+{
+	if (start == end)
+	{
+		return;
+	}
+
+	int mid = (start + end) / 2;
+
+	merge_b(start, mid);
+	merge_b(mid + 1, end);
+
+	int i = start;
+	int j = mid + 1;
+	int q = 0;
+
+	while (i <= mid && j <= end)
+	{
+		if (b[i].x < b[j].x)
+		{
+			c[q].x = b[i].x;
+			c[q].y = b[i].y;
+			++i;
+		}
+
+		else
+		{
+			c[q].x = b[j].x;
+			c[q].y = b[j].y;
+			++j;
+		}
+		q++;
+	}
+
+	while (i <= mid)
+	{
+		c[q].x = b[i].x;
+		c[q].y = b[i].y;
+		i++;
+		q++;
+	}
+
+	while (j <= end)
+	{
+		c[q].x = b[j].x;
+		c[q].y = b[j].y;
+		j++;
+		q++;
+	}
+
+	for (int i = start; i <= end; i++)
+	{
+		b[i].x = c[i - start].x;
+		b[i].y = c[i - start].y;
+	}
+}
+
+void merge_y(int start, int end)
+{
+	while (location <= end)
+	{
+		int k = 0;
+		b[k].x = a[location].x;
+		b[k].y = a[location].y;
+		start = location;
+		k++;
+
+		for (int i = location + 1; i < 1000000; i++)
+		{
+			if (a[location].y == a[i].y)
+			{
+				b[k].x = a[i].x;
+				b[k].y = a[i].y;
+				k++;
+			}
+			else
+			{
+				location = i;
+				break;
+			}
+		}
+
+		merge_b(0, k - 1);
+		for (int i = start; i < start + k; i++)
+		{
+			a[i].x = b[i - start].x;
+			a[i].y = b[i - start].y;
+		}
+	}
+}
+
+
+
+int main()
+{
+
+	int number;
+	scanf("%d", &number);
+
+	for (int i = 0; i < number; i++)
+	{
+		scanf("%d %d", &a[i].x, &a[i].y);
+	}
+
+	merge(0, number - 1);
+	merge_y(0, number - 1);
+
+	for (int i = 0; i < number; i++)
+	{
+		printf("%d %d\n", a[i].x, a[i].y);
+	}
+
+
+
+	return 0;
+}
--- Algorithm Practice/Algorithm Practice/mergesort.cpp
+++ Algorithm Practice/Algorithm Practice/mergesort.cpp
@@ -2,97 +2,57 @@
 #include <stdio.h>
 using namespace std;
 int length = 4;
-int array[4];
+int a[4];
 int b[4];
-
-void comp(int aa, int bb);
 
 void merge(int start, int end)
 {
-	int mid = (start + end) / 2;
-
 	if (start == end)
 	{
 		return;
 	}
 
-	else
+	int mid = (start + end) / 2;
+
+	merge(start, mid);
+	merge(mid + 1, end);
+
+	int i = start;
+	int j = mid + 1;
+	int q = 0;
+
+	while (i <= mid && j <= end)
 	{
-		merge(start, mid);
-		merge(mid + 1, end);
-	}
-
-	comp(start, mid);
-	comp(mid + 1, end);
-
-}
-
-void comp(int start, int end)
-{
-	while (1)
-	{
-		int mid = (start + end) / 2;
-		int i = start;
-		int j = mid + 1;
-		int k = 0;
-
-		printf("initial i is %d, j is %d,start is %d, end is %d\n", i, j,start ,end);
-
-		if (array[i] > array[j])
+		if (a[i] < a[j])
 		{
-			b[k] = array[j];
-			j++;
+			b[q] = a[i];
+			++i;
 		}
+
 		else
 		{
-			b[k] = array[i];
-			i++;
+			b[q] = a[j];
+			++j;
 		}
-		k++;
+		q++;
+	}
 
-		if (i == mid + 1)
-		{			
-			for (int q = j; q < end + 1; q++)
-			{
-				b[k] = array[j];
-				j++;
-				k++;
-				printf("j is %d, k is %d, value is %d\n", j-1,k-1, b[k-1]);
-			}
-			break;
-		}
+	while (i <= mid)
+	{
+		b[q] = a[i];
+		i++;
+		q++;
+	}
 
-		else if (j > end)
-		{
-			for (int w = i; w < mid + 1; w++)
-			{
-				b[k] = array[i];
-				i++;
-				k++;
-				printf("j is %d, k is %d, value is %d\n", i - 1, k - 1, b[k - 1]);
-			}
-			break;
-		}
+	while (j <= end)
+	{
+		b[q] = a[j];
+		j++;
+		q++;
 	}
 
 	for (int i = start; i <= end; i++)
-		array[i] = b[i-start];
-}
-
-void copy()
-{
-	for (int i = 0; i < length; i++)
-		array[i] = b[i];
-}
-int main()
-{
-	array[0] = 7;
-	array[1] = 4;
-	array[2] = 5;
-	array[3] = 3;
-	merge(0, 3);
-	copy();
-
-	printf("sorted value is %d %d %d %d \n", array[0], array[1], array[2], array[3]);
-	return 0;
+	{
+		a[i] = b[i - start];
+	}
 }
